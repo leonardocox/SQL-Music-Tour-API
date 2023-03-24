9b723ec5df8602ffbcee153a48a4a7ebb1e0cf7b
leonardocox <leonardocx0@gmail.com>
2023-03-24 12:10:20 -0400

    homework activity 5

---
+++ migrations/20221082825549-create-band.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const { Sequelize } = require("sequelize");
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("bands", {
+      band_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      genre: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      available_start_time: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      end_time: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("bands");
+  },
+};
